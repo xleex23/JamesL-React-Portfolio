fd101324c20f82966d2630d20f0dbc82aa9254a9
James Lee <jameswlee109@gmail.com>
2020-09-11 14:09:48 -0500

    added animations to landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -6,8 +6,19 @@ class LandingPage extends Component {
     return (
       <>
       <div className="landing-container">
-        <div className="left-side"></div>
-        <div className="right-side"></div>
+        <div className="left-side">
+          <span className="about-tag">ABOUT</span>
+          <div className="break"></div>
+          <span className="adjectives">DEV</span>
+          <span className="adjectives">ELOP</span>
+          <span className="adjectives">ER</span>
+        </div>
+        <div className="right-side">
+          <span className="project-tag">PROJECTS</span>
+          <div className="breaks"></div>
+          <span className="adjective">CREA</span>
+          <span className="adjective">TOR</span>
+        </div>
       </div>
       <img className="profile-img" src={require('../../profilecopy.jpg')} />
       </>
