af2d6380afe2af92c09e23fb82075a236ec80c7e
jonsung89 <jon@bleachr.co>
2020-09-13 00:56:54 -0500

    css cleanup with james

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Nav from '../Nav/Nav';
-import LandingPage from '../../routes/LandingPage/LandingPage';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Nav from "../Nav/Nav";
+import LandingPage from "../../routes/LandingPage/LandingPage";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <>
         <Nav />
-        <main className="middle">
+        <main>
           <Switch>
-            <Route exact path={'/'} component={LandingPage} />
+            <Route exact path={"/"} component={LandingPage} />
           </Switch>
         </main>
       </>
-    )
+    );
   }
 }
 
--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import './LandingPage.css';
+import React, { Component } from "react";
+import "./LandingPage.css";
 
 class LandingPage extends Component {
   render() {
     return (
       <>
-      <div className="landing-container">
-        <div className="left-side">
-          <span className="about-tag">ABOUT</span>
-          <div className="break"></div>
-          <span className="adjectives">DEV</span>
-          <span className="adjectives">ELOP</span>
-          <span className="adjectives">ER</span>
+        <div className="landing-container">
+          {/* Left */}
+          <div className="split left-side">
+            <h2 className="about-tag">ABOUT</h2>
+
+            <div className="adjs developer-container">
+              <span className="adjective">
+                DEVELOPER<span className="dot">.</span>
+              </span>
+              {/* <span className="adjective">DEV</span>
+              <span className="adjective">ELOP</span>
+              <span className="adjective">ER</span> */}
+            </div>
+          </div>
+
+          {/* Center - profile image */}
+          <img className="profile-img" src={require("../../profilecopy.jpg")} />
+
+          {/* Right */}
+          <div className="split right-side">
+            <h2 className="project-tag">PROJECTS</h2>
+
+            <div className="adjs creator-container">
+              <span className="adjective">
+                CREATOR<span className="dot">.</span>
+              </span>
+              {/* <span className="adjective">CREA</span>
+              <span className="adjective">TOR</span> */}
+            </div>
+          </div>
         </div>
-        <div className="right-side">
-          <span className="project-tag">PROJECTS</span>
-          <div className="breaks"></div>
-          <span className="adjective">CREA</span>
-          <span className="adjective">TOR</span>
-        </div>
-      </div>
-      <img className="profile-img" src={require('../../profilecopy.jpg')} />
       </>
-    )
+    );
   }
 }
 
