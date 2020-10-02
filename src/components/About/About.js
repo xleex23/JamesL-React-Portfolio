95985901f39e572cc5d0ee7b12260bdf4a713f41
James Lee <jameswlee109@gmail.com>
2020-10-02 17:01:15 -0500

    about page rough

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './About.css';
+
+class About extends Component {
+  render() {
+    return (
+      <section className="about-text">
+          There are only a few things in life that can replicate the emotion invoked by having a set of code work properly, and I chase those moments.
+          <br></br>
+          Hi! My name is James Lee and I am a passionate software developer.  Throughout my experiences in life, whether personal or professional, I've learned the most important aspect about achieving any goal comes from the appreciation of the process that manifests ideas into life.  Coding has definitely highlighted this aspect and I fell in love with the process instantaneously.  Being a part of a select society that have the ability to create, design, and resolve issues is what drove me to choosing this career path.  Come along with me and let's embark on this exciting journey together...
+      </section>
+    )
+  }
+}
+
+export default About;
--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,36 +1,77 @@
 import React, { Component } from 'react';
 import './LandingPage.css';
+import '../../components/About/About.css';
+import About from '../../components/About/About';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons';
+
 
 class LandingPage extends Component {
+  state = {
+    isOpen: false,
+    leftOpen: false,
+    rightOpen: false
+  }
+
+  handleLeftClick = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+      leftOpen: !this.state.leftOpen
+    })
+  }
+
+  handleRightClick = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+      rightOpen: !this.state.leftOpen
+    })
+  }
+
+  handleChevClick = () => {
+    this.setState({
+      isOpen: false,
+      leftOpen: false,
+      rightOpen: false
+    })
+  }
+
   render() {
+    
+    let chevClass = this.state.isOpen ? "show-chev" : "hide-chev"
+
+    let leftClass = this.state.leftOpen ? "expand-left" : "left-side"
+
     return (
       <>
       <div className="landing-container">
 
-        <div className="left-side">
+        <FontAwesomeIcon className={chevClass} icon={faChevronCircleLeft} onClick={() => this.handleChevClick()} />
+
+        <div className={leftClass} onClick = {()=>this.handleLeftClick()}>
           <span className="about-tag">ABOUT</span>
           <div className="break"></div>
           <span className="adjectives">DEV</span>
           <span className="adjectives">ELOP</span>
           <span className="adjectives">ER</span>
         </div>
+        
+        {this.state.leftOpen ? 
+          ( <About  /> ) : "" 
+        }
 
-        {/* <div className="about-text">
-          There are only a few things in life that can replicate the emotion invoked by having a set of code work properly, and I chase those moments.
-          <br></br>
-          Hi! My name is James Lee and I am a passionate software developer.  Throughout my experiences in life, whether personal or professional, I've learned the most important aspect about achieving any goal comes from the appreciation of the process that manifests ideas into life.  Coding has definitely highlighted this aspect and I fell in love with the process instantaneously.  Being a part of a select society that have the ability to create, design, and resolve issues is what drove me to choosing this career path.  Come along with me and let's embark on this exciting journey together...
-        </div> */}
-
-
+        {!this.state.isOpen && !this.state.leftOpen ? (
         <div className="right-side">
           <span className="project-tag">PROJECTS</span>
           <div className="breaks"></div>
           <span className="adjective">CREA</span>
           <span className="adjective">TOR</span>
-        </div>
+        </div>) : "" }
         
       </div>
-      <img className="profile-img" src={require('../../profilecopy.jpg')} />
+
+      {!this.state.isOpen ? 
+      (<img className="profile-img" src={require('../../profilecopy.jpg')} />) : "" }
+
       </>
     )
   }
